eea0d37b65388f0d3c184f58c206f4b85762f1b3
WILLIAMTHEWEST1 <90918613+WILLIAMTHEWEST1@users.noreply.github.com>
2021-10-12 08:28:43 -0500

    initial commit

--- js/site.js
+++ js/site.js
@@ -1,51 +1,70 @@
-//get starting values from the screen
+//get starting Values from the screen
 //Controller Function
 function getValues() {
-      //Mortgage Calculator
-    //Step 1 - Get User inputs - this will access the DOM and call all other functions
-    function getValues() {
-        let loanAmount = document.getElementById("someId").value;
+    //Get the user input K value - needs to be a number
+    let kValue = "17"; //You need to write the code to get user input, replace this line with a document.getelementbyid.
 
-        let term = document.getElementById("anotherId").value;
+    //convert the input to  a number
+    kValue = Number.parseInt(kValue);
 
-        let interestRate = document.getElementById("thirdId").value;
+    // Make sure I get a number
+    if ( | isNAN(kValue)) {
+        //search the array function
+        let result = checkArray(kValue);
 
-        let calculatedRate = calcRate(interestRate);
-
-        let payment = calcPayment(loanAmount, calculatedRate, term);
-
-        let totalPayments = calcPaymentSchedule(loanAmount, calculatedRate, term, payment);
-
-        displayData(totalPayments);
+    } else {
+        alert("Number array needs numbers.")
 
     }
+}
+function checkArrayA(){
+     const sumArray = [10, 15, 3, 7];
 
-    function getValues2() {
-        let msg = "";
-        //Step 2 - Calculate the interest rate - this is the division by 1200 from the PDF
-        function calcRate() {}
+     //create an output variable
+     let output= false;
 
-        //Step 3 - Calculate monthly payment - Amount times rate divided by the complicated math from the PDF Math.pow()
-        function calcPayment() {}
+     //forward searching - 2 "for loops"
+     for (let index = 0; index <= sumArray.length -2; index++) {
+         for(let j = indext + 1; j <= sumArray.length - 1; j ++)    
+          let outerValue = sumArray.length[index];
+          let innerValue = sumArray[j];
 
-        //Step 3.5 - Calculate the interest rate based on the current balance in rate from step 2
-        //This function is called in Step 4, but you need to build it first
-        //Calculates interest per payment
-        function calcInterest() {}
+          if(outerValue + innerValue == kValue){
+              output = true;
+              return output;
+          }
+     }
 
-        msg = document.getElementById("message").value;
-        //Step 4 - Now that we know the rate and monthly payment we build out our payment schedule
-        //This is the big function of the application - create your object in this function
-        //let obj = {
-        //     month:
-        //     payment:
-        //     principal:
-        //     interest:
-        //     totalInterest:
-        //     balance:
-        // }
-        function calcPaymentSchedule() {}
+
+
+     //or use an Array function - indexOf() - returns the position of a value in an array or -1
+}
+function checkArrayB(kValue){
+
+    const sumArray = [10, 15, 3, 7];
+
+    let output = false;
+
+    for (let index = 0; index < sumArray.length -2; index++) {
+        let checkValue = kValue - sumArray[index];
+        if(sumArray.indexOf(checkValue, index) >= 0){
+            output = true;
+            return output;
+        }
+        
     }
-    //Step 5 - Display all of the calculated information
-    //Use the template structure from FizzBuzz to output your data
-    function displayData() {}
+    
+    return output;
+
+}
+
+function displayData(result){
+    let output = document.getElementById("result");
+
+    if (result) {
+        output.innertext = "Your K value was found."
+    } else {
+        output.innterText = "Your K value was not found."
+    }
+
+}
